a9a8da453576ec9e3c5d285799c749fdd6ae0c9d
wpgwiggum <wpgwiggum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-04 03:36:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hits-ie6-pngfix/trunk@142636 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hits-pngfix.php
+++ hits-pngfix.php
@@ -273,49 +273,49 @@ if (!class_exists('hits_ie6_pngfix')) {
         */
         function admin_options_page() { 
             if($_POST['hits_ie6_pngfix_save']){
-                if (! wp_verify_nonce($_POST['_wpnonce'], 'hits_ie6_pngfix-update-options') ) die('Whoops! There was a problem with the data you posted. Please go back and try again.'); 
+                if (! wp_verify_nonce($_POST['_wpnonce'], 'hits_ie6_pngfix-update-options') ) die(_e('Whoops! There was a problem with the data you posted. Please go back and try again.', $this->localizationDomain)); 
                 $this->options['hits_ie6_pngfix_method'] = $_POST['hits_ie6_pngfix_method'];   
 				$this->options['hits_ie6_pngfix_THM_CSSSelector'] = $_POST['hits_ie6_pngfix_THM_CSSSelector'];
                 $this->saveAdminOptions();
                 
-                echo '<div class="updated"><p>'. __('Success! Your changes were sucessfully saved!') .'</p></div>';
+                echo '<div class="updated"><p>'. __('Success! Your changes were sucessfully saved!', $this->localizationDomain) .'</p></div>';
             }
 ?>                                   
                 <div class="wrap">
                 <h2>HITS- IE6 PNG Fix</h2>
                 <form method="post" id="hits_ie6_pngfix_options">
                 <?php wp_nonce_field('hits_ie6_pngfix-update-options'); 
-				_e('<p>This plugin brought to you for free by <a href="http://www.homeitsolutions.ca/websites/wordpress-plugins/ie6-png-fix">Home I.T. Solutions</a>.</p>');
-                _e('<p>I take no credit for the great effort authors have gone into making each method of getting IE6 PNG compatability to work. I just did the work to merge them all into a single wordpress plugin.</p>');
+				_e('<p>This plugin brought to you for free by <a href="http://www.homeitsolutions.ca/websites/wordpress-plugins/ie6-png-fix">Home I.T. Solutions</a>.</p>', $this->localizationDomain);
+                _e('<p>I take no credit for the great effort authors have gone into making each method of getting IE6 PNG compatability to work. I just did the work to merge them all into a single wordpress plugin.</p>', $this->localizationDomain);
 					?>
                     <table width="100%" cellspacing="2" cellpadding="5" class="form-table"> 
                         <tr valign="top"> 
                             <th width="33%" scope="row"><?php _e('PNG Fix Method:', $this->localizationDomain); ?></th> 
                             <td>
                             <select name="hits_ie6_pngfix_method" id="hits_ie6_pngfix_method" style="width:200px;">
-								<option value="THM1"<?php if (strcmp($this->options['hits_ie6_pngfix_method'],'THM1')==0) { echo ' selected="selected"';} ?>><?php _e('Twin Helix v1.0');?></option>
-								<option value="THM2"<?php if (strcmp($this->options['hits_ie6_pngfix_method'],'THM2')==0) { echo ' selected="selected"';} ?>><?php _e('Twin Helix v2.0');?></option>
-								<option value="UPNGFIX"<?php if (strcmp($this->options['hits_ie6_pngfix_method'],'UPNGFIX')==0) { echo ' selected="selected"';} ?>><?php _e('Unit PNG Fix');?></option>
-								<option value="SUPERSLEIGHT"<?php if (strcmp($this->options['hits_ie6_pngfix_method'],'SUPERSLEIGHT')==0) { echo ' selected="selected"';} ?>><?php _e('SuperSleight');?></option>
+								<option value="THM1"<?php if (strcmp($this->options['hits_ie6_pngfix_method'],'THM1')==0) { echo ' selected="selected"';} ?>><?php _e('Twin Helix v1.0', $this->localizationDomain);?></option>
+								<option value="THM2"<?php if (strcmp($this->options['hits_ie6_pngfix_method'],'THM2')==0) { echo ' selected="selected"';} ?>><?php _e('Twin Helix v2.0', $this->localizationDomain);?></option>
+								<option value="UPNGFIX"<?php if (strcmp($this->options['hits_ie6_pngfix_method'],'UPNGFIX')==0) { echo ' selected="selected"';} ?>><?php _e('Unit PNG Fix', $this->localizationDomain);?></option>
+								<option value="SUPERSLEIGHT"<?php if (strcmp($this->options['hits_ie6_pngfix_method'],'SUPERSLEIGHT')==0) { echo ' selected="selected"';} ?>><?php _e('SuperSleight', $this->localizationDomain);?></option>
                                 
 							</select>
                         </td> 
                         </tr>
                         <tr>
                         	<th width="33%" scope="row"><?php _e('CSS Selector:', $this->localizationDomain); ?></th>
-                            <td><input type="text" name="hits_ie6_pngfix_THM_CSSSelector" value="<?php echo $this->options['hits_ie6_pngfix_THM_CSSSelector'] ?>" size="100" /><br /><?php _e('Note: CSS Selector is only used for the Twin Helix methods.');?></td>
+                            <td><input type="text" name="hits_ie6_pngfix_THM_CSSSelector" value="<?php echo $this->options['hits_ie6_pngfix_THM_CSSSelector'] ?>" size="100" /><br /><?php _e('Note: CSS Selector is only used for the Twin Helix methods.', $this->localizationDomain);?></td>
 						</tr>
                         <tr>
                             <th colspan=2><input type="submit" name="hits_ie6_pngfix_save" value="Save" /></th>
                         </tr>
                     </table>
                     
-                    <p><?php _e('Feedback and requests are always welcome. Visit the plugin website <a href="http://www.homeitsolutions.ca/websites/wordpress-plugins/ie6-png-fix">here</a> to leave any feedback, comments or donations. All donations will go towards micro loans through Kiva at www.kiva.org');?></p>
-                    <h3><?php _e('PNG Fix Credits');?></h3>
-                    <p><?php _e('The Twin Helix approaches were taken from <a href="http://www.twinhelix.com/css/iepngfix/">the twinhelix website</a>.');?></p>
-                    <p><?php _e('The UnitInteractive approach was taken from <a href="http://labs.unitinteractive.com/unitpngfix.php"> the unit interactive labs website</a>.');?></p>
+                    <p><?php _e('Feedback and requests are always welcome.', $this->localizationDomain);?><a href="http://www.homeitsolutions.ca/websites/wordpress-plugins/ie6-png-fix"><?php _e('Visit the plugin website', $this->localizationDomain);?></a> <?php _e('to leave any feedback, translations, comments or donations. All donations will go towards micro loans through', $this->localizationDomain);?> <a href="http://www.kiva.org">Kiva</a>.</p>
+                    <h3><?php _e('PNG Fix Credits', $this->localizationDomain);?></h3>
+                    <p><?php _e('The Twin Helix approaches were taken from', $this->localizationDomain);?> <a href="http://www.twinhelix.com/css/iepngfix/">Twin Helix</a></p>
+                    <p><?php _e('The UnitInteractive approach was taken from', $this->localizationDomain);?> <a href="http://labs.unitinteractive.com/unitpngfix.php"> Unit Interactive Labs</a>.</p>
                     <p><?php _e('I was not able to find the original author for SuperSleight, and just found the code searching google. 
-                       If anyone knows the original author, please let me know so that proper credit can be given.');?></p>
+                       If anyone knows the original author, please let me know so that proper credit can be given.', $this->localizationDomain);?></p>
                 </form>
                 <?php
         }
